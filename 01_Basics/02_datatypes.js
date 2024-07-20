a906f5f4051bfbb09ed2a149cfca2ece01bc89b5
Tusharkaushik17 <123252427+Tusharkaushik17@users.noreply.github.com>
2024-07-20 07:44:05 +0000

    02_datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //this treats all JS code as new version
+
+// alert(3+3) // this works in browser but we are using nodejs
+
+//code readability is priority
+
+console.log(3+3)
+
+console.log("Tushar")
+
+let name = "Tushar" // string datatype
+let age = 18 // int datatype
+let isLoggedIn = false // boolean datatype
+
+// Null => Standalone value and represntation of empty value
+// Undefined => Value is not assigned
+// Symbol => use to define uniqueness
+
+//Object
+
+console.log(typeof"Tushar")
+console.log(typeof"NULL")   //=>Object
