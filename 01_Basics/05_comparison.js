0c001c0825ec226c738e6bbef5a85a81b7e24716
Tusharkaushik17 <123252427+Tusharkaushik17@users.noreply.github.com>
2024-07-20 15:31:01 +0000

    Comparison

---
+++ 01_Basics/05_comparison.js
@@ -0,0 +1,10 @@
+console.log(NULL > 0);
+console.log(NULL == 0);
+console.log(NULL >= 0);
+ // NOTE : comparison(> , >= , < , <=) traets NULL as 0 whereas (==) does not treat NULL as 0
+
+ console.log("2" > 1);
+ console.log("02" > 1);
+
+ // === this is strict check it also check datatype whereas == is not strict check 
+ console.log("2" === 2);
