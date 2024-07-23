14b2611326e338992f173dc39fc18e5cb93e1e5b
Tusharkaushik17 <123252427+Tusharkaushik17@users.noreply.github.com>
2024-07-23 17:06:04 +0000

    object3

---
+++ 02_basics/#5_object3.js
@@ -0,0 +1,31 @@
+const course = {
+    coursename : "JS",
+    coursePrice : "999",
+    courseTeacher : "hitesh"
+}
+
+console.log(course.courseTeacher);
+
+const{courseTeacher : Teacher} = course  // alternate method to print elements of objrct
+
+console.log(Teacher);
+
+
+/////  API INTRODUCTION
+
+// { 
+//     "name" : "tushar",
+//     "subject" : "JS",
+//     "id" : 0001
+// }
+
+
+// API can be array
+
+[
+    {},
+
+    {},
+
+    {}
+]
